7bd6697591a9e645a44b825e46334072ba7bfe0c
suho <devsuho@gmail.com>
2023-08-09 23:01:01 +0900

    2

---
+++ advanced/src/test/java/spring/advanced/trace/hellotrace/HelloTraceV1Test.java
@@ -0,0 +1,23 @@
+package spring.advanced.trace.hellotrace;
+
+import org.junit.jupiter.api.Test;
+import spring.advanced.trace.TraceStatus;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTraceV1Test {
+
+    @Test
+    void log_begin() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("log start");
+        trace.end(status);
+    }
+
+    @Test
+    void log_exception() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("log start");
+        trace.exception(status, new IllegalStateException());
+    }
+ }
