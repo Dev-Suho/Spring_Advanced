32bc8a310623ec7b340dfdc63ccb196fbcf9d7c5
Dev-Suho <devsuho@gmail.com>
2023-08-09 23:14:47 +0900

    HelloTrace

--- advanced/src/main/java/spring/advanced/app/v1/OrderControllerV1.java
+++ advanced/src/main/java/spring/advanced/app/v1/OrderControllerV1.java
@@ -3,12 +3,14 @@ package spring.advanced.app.v1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import spring.advanced.trace.hellotrace.HelloTraceV1;
 
 @RestController
 @RequiredArgsConstructor
 public class OrderControllerV1 {
 
     private final OrderServiceV1 orderService;
+    private final HelloTraceV1 trace;
 
     @GetMapping("/v1/request")
     public String request(String itemId) {
