e69553bfc60950e7a7710f55579759c6769516b0
Dev-suho <devsuho99@gmail.com>
2023-08-11 21:15:56 +0900

    logV2_0811

--- advanced/src/test/java/spring/advanced/trace/hellotrace/HelloTraceV2Test.java
+++ advanced/src/test/java/spring/advanced/trace/hellotrace/HelloTraceV2Test.java
@@ -3,19 +3,23 @@ package spring.advanced.trace.hellotrace;
 import org.junit.jupiter.api.Test;
 import spring.advanced.trace.TraceStatus;
 
-class HelloTraceV1Test {
+class HelloTraceV2Test {
 
     @Test
     void log_begin() {
-        HelloTraceV1 trace = new HelloTraceV1();
+        HelloTraceV2 trace = new HelloTraceV2();
         TraceStatus status = trace.begin("log start!");
+        TraceStatus status2 = trace.beginSync(status.getTraceId(), "log level2");
+        trace.end(status2);
         trace.end(status);
     }
 
     @Test
     void log_exception() {
-        HelloTraceV1 trace = new HelloTraceV1();
+        HelloTraceV2 trace = new HelloTraceV2();
         TraceStatus status = trace.begin("log start!");
+        TraceStatus status2 = trace.beginSync(status.getTraceId(), "log level2");
+        trace.exception(status2, new IllegalStateException());
         trace.exception(status, new IllegalStateException());
     }
  }
